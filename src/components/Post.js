ae294da92da6cb5c952e399827af6cb627fea2fe
“winiciuskyutoku“ <winicius123098@gmail.com>
2023-02-10 23:14:41 -0300

    Like on Doubleclick and white heart animation included

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,16 +5,19 @@ export default function Post(props) {
     const [liked, setLiked] = React.useState("heart-outline")
     const [none, setNone] = React.useState("")
     const [numLikes, setnumLikes] = React.useState(props.likes)
+    const [animation, setAnimation] = React.useState("displayNone")
 
     function markAsLiked(item){
         if (liked === "heart-outline"){
             setLiked("heart");
             setNone("liked");
-            setnumLikes(numLikes + 1)
+            setnumLikes(numLikes + 1) 
+            setAnimation("animation");
         } else if (liked === "heart" && item !== "image"){
             setLiked("heart-outline");
             setNone("");
             setnumLikes(numLikes - 1)
+            setAnimation("displayNone")
         }
     }
 
@@ -39,14 +42,15 @@ export default function Post(props) {
             </div>
 
             <div className="conteudo">
-                <img onClick={() => markAsLiked("image")} src={props.content} alt={props.contentName} />
+                <img onDoubleClick={() => markAsLiked("image")} src={props.content} alt={props.contentName} />
+                <ion-icon name="heart" class={animation}></ion-icon>
             </div>
 
             <div className="fundo">
                 <div className="acoes">
                     <div>
                         <ion-icon name={liked} class={none} onClick={markAsLiked}></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
+                        <ion-icon name="chatbubble-outline" ></ion-icon>
                         <ion-icon name="paper-plane-outline"></ion-icon>
                     </div>
                     <div>
